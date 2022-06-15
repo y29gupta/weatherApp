56e93e5eb684bfcef805473a12bfc8198d086fce
yash <yash.gupta7754@gmail.com>
2022-06-15 12:39:12 +0530

    cimmited

--- src/component/Temp.js
+++ src/component/Temp.js
@@ -84,77 +84,68 @@ function Temp() {
 
     return (
         <>
-            <div className="container-fluid col-md-12 ">
+            <div className="container-fluid ">
                 <div className="row col-md-8  m-auto justify-content-center">
-                    <div className="col-md-4 mt-5 mb-2">
-                        <div className="form-outline">
+                    <div className="col-md- col-6 bg-dark mt-5 mb-2">
+                        <div className=" form-outline  ">
 
                             <input type="text" className='form-control form-control-sm' placeholder='search' onChange={(e) => setSearching(e.target.value)} value={searching} />
                         </div>
                     </div>
-                    <div className="col-md-2 mt-5 mb-2">
+                    <div className="col-md-4 col-4 mt-5 mb-2">
                         <button className="btn btn-sm btn-primary" onClick={getData}>Search</button>
 
                     </div>
                 </div>
-                <div className="row col-md-8 m-auto">
+                <div className="row col-md-8 col-12 m-auto">
                     <div className="card">
                         <div className="card-body ">
 
                             <div className=" heading fs-5 text-center p-3 text-dark">Check the weather forecast</div>
-                            <div className="col-md-12 iconbox">
+                            <div className=" col-12 iconbox">
                                 <div className="col p-4 text-center">
                                     <i className={`wi ${weathericon} logo`}></i>
                                 </div>
 
                             </div>
-                            <div className="col-md-12 bg-light d-flex flex-row  ">
-                                <div className="col-md-8 bg-dark d-flex flex-row">
-                                    {/* <div className="col mt-5">
-
-                                    <strong >
-                                    <h4 className="d-inline-block  display-4 text-white  ps-1">{tempdata.temp} &deg;</h4>
-                                <h4 className='d-inline-block ps-5  display-4 text-white'>{tempdata.weatherType}</h4>
-                                <div className="col-2 offset-3">
-                                <p className='text-white' >{tempdata.name},{tempdata.country}</p>
-                                </div>
-                                    </strong>
-                                </div> */}
-                                    <div className="col-5">
+                            <div className=" col-12 bg-info d-flex flex-row  ">
+                                <div className="col-md-8 col-6 bg-dark d-flex flex-md-row flex-column">
+                                    
+                                    <div className=" col-md-5 col-12">
                                         <strong><h3 className="display-3 text-center text-white p-4 ">{tempdata.temp}&deg;</h3></strong>
                                     </div>
-                                    <div className="col-5 bg-secondary">
-                                        <strong><h3 className=" display-3 text-white  p-4 ps-2 ">{tempdata.weatherType}</h3>
-                                            <p className='text-white ps-2'>{tempdata.name} , {tempdata.country}</p>
+                                    <div className=" col-md-5 col-12 bg-secondary">
+                                        <strong><h3 className=" display-3 text-white  p-4  text-center  ">{tempdata.weatherType}</h3>
+                                            <p className='text-white text-center'>{tempdata.name} , {tempdata.country}</p>
                                         </strong>
                                     </div>
 
 
 
                                 </div>
-                                <div className="position-relative col-md-4 timecol ">
+                                <div className="position-relative col-6 col-md-4 timecol ">
                                     <div className="date">{date}</div>
                                 </div>
                             </div>
                             
 
-                            <div className="col-md-12 d-flex flex-row justify-content-between" id='card-footer'>
-                                <div className="col-3  text-center">
+                            <div className="col-12 d-md-flex flex-md-row justify-content-between" id='card-footer'>
+                                <div className="col-md-3 col-12  text-center">
                                     <p className='d-inline-block'><i className={'wi wi-sunset text-dark'} ></i>
                                     </p>
                                     <p className='d-inline-block ps-3 '>{sunsetTime} PM<br /> sunset</p>
                                 </div>
-                                <div className="col-3 text-center">
+                                <div className="col-md-3 col-12 text-center">
                                     <p className='d-inline-block'><i className={'wi wi-humidity text-dark'} ></i>
                                     </p>
                                     <p className='d-inline-block ps-3 '>{tempdata.humidity} <br /> Humidity</p>
                                 </div>
-                                <div className="col-3 text-center">
+                                <div className="col-md-3 col-12 text-center">
                                     <p className='d-inline-block'><i className={'wi wi-day-rain text-dark'} ></i>
                                     </p>
                                     <p className='d-inline-block ps-3 '>{tempdata.pressure} <br /> pressure</p>
                                 </div>
-                                <div className="col-3 text-center ">
+                                <div className="col-md-3 col-12 text-center ">
                                     <p className='d-inline-block'><i className={'wi wi-day-windy text-dark'} ></i>
                                     </p>
                                     <p className='d-inline-block ps-3 '>{tempdata.speed} <br /> speed</p>
